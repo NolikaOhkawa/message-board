a517da85ca6ee57975d26db4e23c6c57b89eb73d
nolikaohkawa <s10124no@gmail.com>
2015-10-28 16:35:29 +0000

    generate migration AddAge

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
     # 名前は必須入力かつ20文字以内
     validates :name , length: {minimum: 1 , maximum: 20 , too_short: '名前を入力してください' , too_long: '名前が長すぎます' } , presence: true
+    # 年齢は必須。数値で5以上
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 5 }, presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 1 , maximum: 30 , too_short: '内容を入力してください' , too_long: 'もっと簡潔に述べてください' } , presence: true
 end
